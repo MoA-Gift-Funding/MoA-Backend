b2bbd0bb1cab573e4711e541dd4581e65229df4e
Donghun Shin <huipulci1@naver.com>
2024-01-22 01:05:39 +0900

    [MOA-254] 회원가입 전 회원 수정 API 호출 시 예외처리 (#3)
    * MOA-254: 최종 회원가입 전 회원 수정 API 호출 시 예외
    
    * MOA-254: API 문서 업데이트

--- src/main/java/moa/member/exception/MemberExceptionType.java
+++ src/main/java/moa/member/exception/MemberExceptionType.java
@@ -1,5 +1,6 @@
 package moa.member.exception;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
@@ -17,6 +18,8 @@ public enum MemberExceptionType implements MoaExceptionType {
     NOT_VERIFIED_PHONE(FORBIDDEN, "핸드폰 검증이 이루어지지 않았습니다."),
     FAILED_SEND_PHONE_VERIFICATION_NUMBER(INTERNAL_SERVER_ERROR, "핸드폰 인증번호 발송에 실패했습니다."),
     DIFFERENT_PHONE_VERIFICATION_NUMBER(UNAUTHORIZED, "핸드폰 인증번호가 일치하지 않습니다."),
+    ALREADY_SIGNED_UP(BAD_REQUEST, "이미 회원가입한 회원입니다."),
+    NO_AUTHORITY_FOR_NOT_SIGNED_UP(FORBIDDEN, "임시가입 상태이거나 탈퇴한 회원에게는 권한이 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
