919347fe12812cfff28ab54ed15f4c0fcd7cb06d
shin-mallang <huipulci1@naver.com>
2024-01-22 00:06:08 +0900

    MOA-253: 내 친구 목록 조회 API 구현

--- src/main/java/moa/friend/presentation/FriendApi.java
+++ src/main/java/moa/friend/presentation/FriendApi.java
@@ -1,14 +1,19 @@
 package moa.friend.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import moa.friend.presentation.request.SyncContactRequest;
+import moa.friend.query.response.FriendResponse;
 import moa.global.auth.Auth;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Tag(name = "친구 API", description = "친구 관련 API")
@@ -23,9 +28,24 @@ public interface FriendApi {
                     @ApiResponse(responseCode = "404"),
             }
     )
+    @Operation(summary = "연락처 동기화")
     @PostMapping("/sync-contact")
     ResponseEntity<Void> syncContact(
             @Parameter(hidden = true) @Auth Long memberId,
             @Schema SyncContactRequest request
+    );
+
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200"),
+                    @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(hidden = true))),
+                    @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(hidden = true))),
+                    @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(hidden = true))),
+            }
+    )
+    @Operation(summary = "내 친구 목록 조회")
+    @GetMapping("/my")
+    ResponseEntity<List<FriendResponse>> findMyFriends(
+            @Parameter(hidden = true) @Auth Long memberId
     );
 }
--- src/main/java/moa/friend/presentation/FriendController.java
+++ src/main/java/moa/friend/presentation/FriendController.java
@@ -1,10 +1,14 @@
 package moa.friend.presentation;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import moa.friend.application.FriendService;
 import moa.friend.presentation.request.SyncContactRequest;
+import moa.friend.query.FriendQueryService;
+import moa.friend.query.response.FriendResponse;
 import moa.global.auth.Auth;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FriendController implements FriendApi {
 
     private final FriendService friendService;
+    private final FriendQueryService friendQueryService;
 
     @PostMapping("/sync-contact")
     public ResponseEntity<Void> syncContact(
@@ -24,5 +29,11 @@ public class FriendController implements FriendApi {
     ) {
         friendService.makeFromContact(request.toCommand(memberId));
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/my")
+    public ResponseEntity<List<FriendResponse>> findMyFriends(@Auth Long memberId) {
+        List<FriendResponse> result = friendQueryService.findFriendsByMemberId(memberId);
+        return ResponseEntity.ok(result);
     }
 }
