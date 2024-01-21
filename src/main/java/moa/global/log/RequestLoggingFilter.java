8f3d58b3d8607bfdfa1c6eebeb6d80012de9e180
shin-mallang <huipulci1@naver.com>
2024-01-22 00:49:18 +0900

    MOA-247: 요청, 응답 로그 간단하게 남기기

---
+++ src/main/java/moa/global/log/RequestLoggingFilter.java
@@ -0,0 +1,100 @@
+package moa.global.log;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jboss.logging.MDC;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.PathMatcher;
+import org.springframework.util.StopWatch;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+import org.springframework.web.util.WebUtils;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class RequestLoggingFilter implements Filter {
+
+    public static final String REQUEST_ID = "requestId";
+
+    private final ObjectProvider<PathMatcher> pathMatcher;
+    private final Set<String> setIgnoredUrlPatterns = new HashSet<>();
+
+    public void setIgnoredUrlPatterns(String... ignoredUrlPatterns) {
+        this.setIgnoredUrlPatterns.addAll(Arrays.asList(ignoredUrlPatterns));
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        if (CorsUtils.isPreFlightRequest(httpRequest) || isIgnoredUrl(httpRequest)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        ContentCachingRequestWrapper cachedRequest = new ContentCachingRequestWrapper((HttpServletRequest) request);
+        ContentCachingResponseWrapper cachedResponse = new ContentCachingResponseWrapper(
+                (HttpServletResponse) response);
+
+        StopWatch stopWatch = new StopWatch();
+        try {
+            MDC.put(REQUEST_ID, getRequestId(httpRequest));
+            stopWatch.start();
+            log.info("[{}] request start [uri: {} {}]",
+                    MDC.get(REQUEST_ID), httpRequest.getMethod(), httpRequest.getRequestURI());
+            chain.doFilter(cachedRequest, cachedResponse);
+        } finally {
+            stopWatch.stop();
+            log.info("[{}] request end [time: {}ms]", MDC.get(REQUEST_ID), stopWatch.getTotalTimeMillis());
+            log.info("[{}] response body: {}", MDC.get(REQUEST_ID), getResponseBody(cachedResponse));
+            MDC.clear();
+        }
+    }
+
+    private boolean isIgnoredUrl(HttpServletRequest request) {
+        PathMatcher pathMatcher = this.pathMatcher.getIfAvailable();
+        Objects.requireNonNull(pathMatcher);
+        return setIgnoredUrlPatterns.stream()
+                .anyMatch(pattern -> pathMatcher.match(pattern, request.getRequestURI()));
+    }
+
+    private String getRequestId(HttpServletRequest httpRequest) {
+        String requestId = httpRequest.getHeader("X-Request-ID");
+        if (ObjectUtils.isEmpty(requestId)) {
+            return UUID.randomUUID().toString().replaceAll("-", "");
+        }
+        return requestId;
+    }
+
+    private String getResponseBody(HttpServletResponse response) throws IOException {
+        String payload = null;
+        ContentCachingResponseWrapper wrapper =
+                WebUtils.getNativeResponse(response, ContentCachingResponseWrapper.class);
+        if (wrapper != null) {
+            wrapper.setCharacterEncoding("UTF-8");
+            byte[] buf = wrapper.getContentAsByteArray();
+            if (buf.length > 0) {
+                payload = new String(buf, wrapper.getCharacterEncoding());
+                wrapper.copyBodyToResponse();
+            }
+        }
+        return null == payload ? " - " : payload;
+    }
+}
---
+++ src/main/java/moa/global/log/WebLogConfig.java
@@ -0,0 +1,29 @@
+package moa.global.log;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebLogConfig {
+
+    private final RequestLoggingFilter requestLoggingFilter;
+
+    @Bean
+    public FilterRegistrationBean<RequestLoggingFilter> firstFilter() {
+        FilterRegistrationBean<RequestLoggingFilter> registrationBean = new FilterRegistrationBean<>();
+        requestLoggingFilter.setIgnoredUrlPatterns(
+                "/error",
+                "/favicon.ico",
+                "/api-docs/**",
+                "/swagger-ui/**"
+        );
+        registrationBean.setFilter(requestLoggingFilter);
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(1);
+        registrationBean.setName("requestLoggingFilter");
+        return registrationBean;
+    }
+}
