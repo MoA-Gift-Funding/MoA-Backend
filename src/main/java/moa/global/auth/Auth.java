11432a37d3b83d4946113b5df21460792175198c
shin-mallang <huipulci1@naver.com>
2024-01-22 22:46:05 +0900

    MOA-266: @Auth 의 속성으로 허락할 회원 상태 적용

--- src/main/java/moa/global/auth/Auth.java
+++ src/main/java/moa/global/auth/Auth.java
@@ -5,8 +5,11 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+import moa.member.domain.MemberStatus;
 
 @Target(PARAMETER)
 @Retention(RUNTIME)
 public @interface Auth {
+
+    MemberStatus[] permit();
 }
--- src/main/java/moa/global/auth/AuthExceptionType.java
+++ src/main/java/moa/global/auth/AuthExceptionType.java
@@ -7,6 +7,7 @@ public enum AuthExceptionType implements MoaExceptionType {
 
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 유효하지 않습니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증되지 않았습니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
