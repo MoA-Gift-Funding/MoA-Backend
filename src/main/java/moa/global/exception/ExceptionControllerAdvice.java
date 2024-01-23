f0107bede68e84373f25a773aceda73157d51a99
최준호 <junho5336@gmail.com>
2024-01-23 22:49:33 +0900

    MOA-268: MethodArgumentNotValidException 핸들링 추가

--- src/main/java/moa/global/exception/ExceptionControllerAdvice.java
+++ src/main/java/moa/global/exception/ExceptionControllerAdvice.java
@@ -1,45 +1,47 @@
 package moa.global.exception;
 
-import static moa.global.log.RequestLoggingFilter.REQUEST_ID;
-
-import jakarta.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
 import org.jboss.logging.MDC;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.WebUtils;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import static moa.global.log.RequestLoggingFilter.REQUEST_ID;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
 
-    @ExceptionHandler(MoaException.class)
+    @ExceptionHandler({MoaException.class, MethodArgumentNotValidException.class})
     ResponseEntity<ExceptionResponse> handleException(HttpServletRequest request, MoaException e) {
         MoaExceptionType type = e.getExceptionType();
         log.info("[{}] 잘못된 요청이 들어왔습니다. uri: {} {},  내용:  {}",
-                MDC.get(REQUEST_ID), request.getMethod(), request.getRequestURI(), type.getMessage());
+            MDC.get(REQUEST_ID), request.getMethod(), request.getRequestURI(), type.getMessage());
         log.info("[{}] request header: {}", MDC.get(REQUEST_ID), getHeaders(request));
         log.info("[{}] request body: {}", MDC.get(REQUEST_ID), getRequestBody(request));
         return ResponseEntity.status(type.getHttpStatus())
-                .body(new ExceptionResponse(type.getMessage()));
+            .body(new ExceptionResponse(type.getMessage()));
     }
-
+    
     @ExceptionHandler(Exception.class)
     ResponseEntity<ExceptionResponse> handleException(HttpServletRequest request, Exception e) {
         log.error("[{}] 예상하지 못한 예외가 발생했습니다. uri: {} {}, ",
-                MDC.get(REQUEST_ID), request.getMethod(), request.getRequestURI(), e);
+            MDC.get(REQUEST_ID), request.getMethod(), request.getRequestURI(), e);
         log.info("[{}] request header: {}", MDC.get(REQUEST_ID), getHeaders(request));
         log.info("[{}] request body: {}", MDC.get(REQUEST_ID), getRequestBody(request));
         return ResponseEntity.internalServerError()
-                .body(new ExceptionResponse("알 수 없는 오류가 발생했습니다."));
+            .body(new ExceptionResponse("알 수 없는 오류가 발생했습니다."));
     }
 
     private Map<String, Object> getHeaders(HttpServletRequest request) {
