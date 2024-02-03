fcc5018520e33d2830177dc73a1762fdfe93fe6c
shin-mallang <huipulci1@naver.com>
2024-02-03 11:54:17 +0900

    MOA-305: reflection 의존성 지우기

--- src/main/java/moa/global/exception/ExceptionDocGenerator.java
+++ src/main/java/moa/global/exception/ExceptionDocGenerator.java
@@ -1,25 +1,52 @@
 package moa.global.exception;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Set;
-import org.reflections.Reflections;
-import org.reflections.scanners.SubTypesScanner;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 
 public class ExceptionDocGenerator {
 
-    public static void main(String[] args) throws Exception {
-        Reflections reflections = new Reflections(new ConfigurationBuilder()
-                .setUrls(ClasspathHelper.forPackage("moa"))
-                .setScanners(new SubTypesScanner(false)));
-        Set<Class<? extends MoaExceptionType>> subTypesOf = reflections.getSubTypesOf(MoaExceptionType.class);
-        // resources의 static 폴더 하위에 생성되게 해줘
+    public static void main(String[] args) {
+        writeDocs(getExceptionTypes());
+    }
+
+    private static List<Class<?>> getExceptionTypes() {
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        File moa = new File(classLoader.getResource("moa").getFile());
+        List<Class<?>> classes = new ArrayList<>();
+        readClass(moa.getAbsolutePath(), moa.listFiles(), classes);
+        return classes.stream()
+                .filter(MoaExceptionType.class::isAssignableFrom)
+                .filter(it -> !it.isInterface())
+                .toList();
+    }
+
+    private static void readClass(String rootPath, File[] files, List<Class<?>> store) {
+        for (File file : files) {
+            if (file.isDirectory()) {
+                readClass(rootPath, file.listFiles(), store);
+            } else {
+                String replace = file.getAbsolutePath()
+                        .replace(rootPath, "")
+                        .replace("/", ".")
+                        .replace(".class", "")
+                        .replaceFirst("\\.", "moa.");
+                try {
+                    store.add(Class.forName(replace));
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    private static void writeDocs(List<Class<?>> subTypesOf) {
         Path outputPath = Paths.get("src", "main", "resources", "static", "exception_doc.html");
         try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(outputPath))) {
             writer.println("""
@@ -30,7 +57,7 @@ public class ExceptionDocGenerator {
                         </head>
                     <body>
                     """);
-            for (Class<? extends MoaExceptionType> aClass : subTypesOf) {
+            for (Class<?> aClass : subTypesOf) {
                 writer.println("<h2>" + aClass.getSimpleName() + "</h2>");
                 writer.println("""
                         <table>
@@ -42,8 +69,7 @@ public class ExceptionDocGenerator {
                         """);
 
                 for (Object enumConstant : aClass.getEnumConstants()) {
-                    if (enumConstant instanceof MoaExceptionType) {
-                        MoaExceptionType exceptionType = (MoaExceptionType) enumConstant;
+                    if (enumConstant instanceof MoaExceptionType exceptionType) {
                         HttpStatus httpStatus = exceptionType.getHttpStatus();
                         String message = exceptionType.getMessage();
                         String name = exceptionType.name();
