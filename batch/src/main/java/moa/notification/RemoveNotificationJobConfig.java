47cffc6db16b74e8d498f15e4ea68812899fc6a1
최준호 <junho5336@gmail.com>
2024-02-16 02:58:25 +0900

    MOA-336: 15일 지난 푸시 알림 제거

---
+++ batch/src/main/java/moa/notification/RemoveNotificationJobConfig.java
@@ -0,0 +1,68 @@
+package moa.notification;
+
+
+import static moa.Crons.EVERY_MIDNIGHT;
+import static org.springframework.batch.repeat.RepeatStatus.FINISHED;
+
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class RemoveNotificationJobConfig {
+
+    private final JobLauncher jobLauncher;
+    private final JdbcTemplate jdbcTemplate;
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+
+    @Scheduled(cron = EVERY_MIDNIGHT)
+    public void launch() throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLocalDateTime("now", LocalDateTime.now())
+                .toJobParameters();
+        jobLauncher.run(removeNotificationJob(), jobParameters);
+    }
+
+    @Bean
+    public Job removeNotificationJob() {
+        return new JobBuilder("removeNotificationJob", jobRepository)
+                .start(removeNotificationStep(null))
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step removeNotificationStep(
+            @Value("#{jobParameters[now]}") LocalDateTime now
+    ) {
+        log.info("[removeNotificationStep] execute [now: {}]", now);
+        return new StepBuilder("removeNotificationStep", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    int updated = jdbcTemplate.update("""
+                            DELETE FROM notification
+                            WHERE created_date <= ?
+                            """, now.minusDays(16));
+                    log.info("삭제된 알림 갯수: {}", updated);
+                    return FINISHED;
+                }, transactionManager)
+                .build();
+    }
+}
---
+++ batch/src/test/java/moa/notification/RemoveNotificationJobConfigTest.java
@@ -0,0 +1,88 @@
+package moa.notification;
+
+import static moa.fixture.MemberFixture.member;
+import static moa.member.domain.MemberStatus.SIGNED_UP;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.batch.core.BatchStatus.COMPLETED;
+
+import java.time.LocalDateTime;
+import moa.BatchTest;
+import moa.member.domain.MemberRepository;
+import moa.notification.domain.NotificationRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
+
+@BatchTest
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(ReplaceUnderscores.class)
+class RemoveNotificationJobConfigTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    @Autowired
+    private Job removeNotificationJob;
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+    private TransactionTemplate transactionTemplate;
+
+    @BeforeEach
+    void setUp() {
+        jobLauncherTestUtils.setJob(removeNotificationJob);
+        transactionTemplate = new TransactionTemplate(transactionManager);
+    }
+
+    @Test
+    void 알림_제거_15일이_지난_알림만_제거된다() throws Exception {
+        jobLauncherTestUtils.setJob(removeNotificationJob);
+        // given
+        var member = memberRepository.save(member(null, "1", "010-1111-1111", SIGNED_UP));
+        LocalDateTime now = LocalDateTime.of(2024, 1, 20, 0, 0, 0);
+
+        jdbcTemplate.update("""
+                INSERT INTO notification(url, title, message, member_id, created_date, is_read)
+                VALUES ('testUrl', 'testTitle', 'testMessage', ?, ?, false)
+                """, member.getId(), LocalDateTime.of(2024, 1, 4, 0, 0, 0)); // 삭제 대상
+        jdbcTemplate.update("""
+                INSERT INTO notification(url, title, message, member_id, created_date, is_read)
+                VALUES ('testUrl', 'testTitle', 'testMessage', ?, ?, false)
+                """, member.getId(), LocalDateTime.of(2024, 1, 5, 0, 0, 0));
+        jdbcTemplate.update("""
+                INSERT INTO notification(url, title, message, member_id, created_date, is_read)
+                VALUES ('testUrl', 'testTitle', 'testMessage', ?, ?, false)
+                """, member.getId(), LocalDateTime.of(2024, 1, 6, 0, 0, 0));
+        // when
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLocalDateTime("now", now)
+                .toJobParameters();
+
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+        // then
+        assertThat(jobExecution.getStatus()).isEqualTo(COMPLETED);
+        Integer count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM notification", Integer.class);
+        assertThat(count).isEqualTo(2);
+    }
+}
