0e0702bc93b41ab811b5b4d1a68570b1da3df348
shin-mallang <huipulci1@naver.com>
2024-02-16 11:56:56 +0900

    MOA-366: 펀딩의 알림 관련 이벤트를 처리하는 핸들러 구현

--- core/src/main/java/moa/funding/application/FundingNotificationEventHandler.java
+++ core/src/main/java/moa/funding/application/FundingNotificationEventHandler.java
@@ -1,5 +1,6 @@
 package moa.funding.application;
 
+
 import static moa.global.config.async.AsyncConfig.VIRTUAL_THREAD_EXECUTOR;
 import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW;
 import static org.springframework.transaction.event.TransactionPhase.AFTER_COMMIT;
@@ -11,10 +12,13 @@ import moa.friend.domain.Friend;
 import moa.friend.domain.FriendRepository;
 import moa.funding.domain.Funding;
 import moa.funding.domain.FundingCreateEvent;
+import moa.funding.domain.FundingFinishEvent;
+import moa.funding.domain.FundingRepository;
 import moa.member.domain.Member;
 import moa.notification.application.NotificationService;
 import moa.notification.domain.Notification;
 import moa.notification.domain.NotificationFactory;
+import moa.product.domain.Product;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,16 +26,17 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 @Service
 @RequiredArgsConstructor
-public class FundingEventHandler {
+@Async(VIRTUAL_THREAD_EXECUTOR)
+@Transactional(propagation = REQUIRES_NEW)
+public class FundingNotificationEventHandler {
 
+    private final FundingRepository fundingRepository;
     private final FriendRepository friendRepository;
-    private final NotificationService notificationService;
     private final NotificationFactory notificationFactory;
+    private final NotificationService notificationService;
 
-    @Async(VIRTUAL_THREAD_EXECUTOR)
-    @Transactional(propagation = REQUIRES_NEW)
     @TransactionalEventListener(value = FundingCreateEvent.class, phase = AFTER_COMMIT)
-    public void notifyCreatedFundingToFriends(FundingCreateEvent event) {
+    public void push(FundingCreateEvent event) {
         Funding funding = event.funding();
         Member fundingOwner = funding.getMember();
         List<Friend> friendsUnblockedOwner = friendRepository.findUnblockedByTargetId(fundingOwner);
@@ -39,7 +44,7 @@ public class FundingEventHandler {
         List<Member> unblockedFriends = getUnblockedFriends(friendsUnblockedOwner, ownersUnblockedFriends);
         List<Notification> notifications = unblockedFriends.stream()
                 .map(target -> notificationFactory.generateFundingCreatedNotification(
-                        getNickName(fundingOwner, friendsUnblockedOwner),
+                        getNickName(target, friendsUnblockedOwner),
                         funding.getTitle(),
                         funding.getProduct().getImageUrl(),
                         funding.getId(),
@@ -61,11 +66,25 @@ public class FundingEventHandler {
         return unblocked;
     }
 
-    private String getNickName(Member owner, List<Friend> friendsUnblockedOwner) {
+    private String getNickName(Member target, List<Friend> friendsUnblockedOwner) {
         return friendsUnblockedOwner.stream()
-                .filter(friend -> friend.getTarget().equals(owner))
+                .filter(friend -> friend.getMember().equals(target))
                 .findAny()
                 .map(Friend::getNickname)
-                .orElseGet(owner::getNickname);
+                .orElseGet(target::getNickname);
+    }
+
+
+    @TransactionalEventListener(value = FundingFinishEvent.class, phase = AFTER_COMMIT)
+    public void push(FundingFinishEvent event) {
+        Funding funding = fundingRepository.getById(event.fundingId());
+        Product product = funding.getProduct();
+        Notification notification = notificationFactory.generateFundingFinishNotification(
+                funding.getTitle(),
+                product.getImageUrl(),
+                funding.getId(),
+                funding.getMember()
+        );
+        notificationService.push(notification);
     }
 }
