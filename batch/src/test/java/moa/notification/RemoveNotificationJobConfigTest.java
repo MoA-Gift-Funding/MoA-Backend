63a2eb74708d9639a1934f89fd1281d6c0522e45
최준호 <junho5336@gmail.com>
2024-02-20 20:41:42 +0900

    MOA-364: merge develop

--- batch/src/test/java/moa/notification/RemoveNotificationJobConfigTest.java
+++ batch/src/test/java/moa/notification/RemoveNotificationJobConfigTest.java
@@ -4,7 +4,6 @@ import static moa.fixture.MemberFixture.member;
 import static moa.member.domain.MemberStatus.SIGNED_UP;
 import static moa.notification.domain.NotificationType.PARTY;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.batch.core.BatchStatus.COMPLETED;
 
 import java.time.LocalDateTime;
 import moa.BatchTest;
@@ -22,7 +21,6 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.util.ReflectionTestUtils;
 
 @BatchTest
@@ -32,9 +30,6 @@ class RemoveNotificationJobConfigTest {
 
     @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
 
     @Autowired
     private MemberRepository memberRepository;
@@ -52,7 +47,6 @@ class RemoveNotificationJobConfigTest {
 
     @Test
     void 알림_제거_15일이_지난_알림만_제거된다() throws Exception {
-        jobLauncherTestUtils.setJob(removeNotificationJob);
         // given
         var member = memberRepository.save(member(null, "1", "010-1111-1111", SIGNED_UP));
         LocalDateTime now = LocalDateTime.of(2024, 1, 20, 0, 0, 0);
@@ -68,7 +62,6 @@ class RemoveNotificationJobConfigTest {
         JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
 
         // then
-        assertThat(jobExecution.getStatus()).isEqualTo(COMPLETED);
         Integer count = notificationRepository.findAll().size();
         assertThat(count).isEqualTo(2);
     }
