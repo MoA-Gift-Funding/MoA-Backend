1038f887dc84b5ccec1e5ce26c11ccc19ef16d2a
최준호 <junho5336@gmail.com>
2024-02-21 13:21:22 +0900

    MOA-364: 불필요한 테스트 제거

--- batch/src/test/java/moa/funding/FundingExpireJobConfigTest.java
+++ batch/src/test/java/moa/funding/FundingExpireJobConfigTest.java
@@ -1,21 +1,15 @@
 package moa.funding;
 
-import static moa.fixture.FundingFixture.funding;
-import static moa.fixture.MemberFixture.member;
 import static moa.fixture.ProductFixture.product;
 import static moa.funding.domain.FundingStatus.EXPIRED;
 import static moa.funding.domain.FundingStatus.PROCESSING;
-import static moa.member.domain.MemberStatus.SIGNED_UP;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import static org.springframework.batch.core.BatchStatus.COMPLETED;
-import static org.springframework.test.util.ReflectionTestUtils.setField;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import moa.BatchTest;
-import moa.friend.domain.Friend;
 import moa.friend.domain.FriendRepository;
 import moa.funding.domain.Funding;
 import moa.funding.domain.FundingRepository;
@@ -110,34 +104,6 @@ class FundingExpireJobConfigTest {
         );
     }
 
-    @Test
-    void 만료된_펀딩에_대해_알림_발송() throws Exception {
-        // given
-        Member owner = memberRepository.save(member(null, "1", "010-1111-1111", SIGNED_UP));
-        Member part = memberRepository.save(member(null, "1", "010-1111-1111", SIGNED_UP));
-        friendRepository.save(new Friend(owner, part, "1"));
-        friendRepository.save(new Friend(part, owner, "1"));
-
-        // 24년 1월 20일 00시 00분 기준
-        LocalDateTime now = LocalDateTime.of(2024, 1, 20, 20, 0, 0);
-        var 만료_1일차 = 펀딩_생성(owner, LocalDate.of(2024, 1, 19)); // 알림 발송 대상
-        var 만료_2일차 = 펀딩_생성(owner, LocalDate.of(2024, 1, 18));
-        var 진행중 = 펀딩_생성(owner, LocalDate.of(2024, 1, 20));
-        var 진행중2 = 펀딩_생성(owner, LocalDate.of(2024, 1, 21));
-
-        // when
-        JobParameters jobParameters = new JobParametersBuilder()
-                .addLocalDateTime("now", now)
-                .toJobParameters();
-
-        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
-
-        // then
-        assertThat(jobExecution.getStatus()).isEqualTo(COMPLETED);
-        assertThat(notificationRepository.findAll()).hasSize(1);
-    }
-
-
     private Funding 펀딩_생성(LocalDate expireDate, Member member, Product product) {
         return fundingRepository.save(
                 new Funding(
@@ -152,18 +118,5 @@ class FundingExpireJobConfigTest {
                         null,
                         ""
                 ));
-    }
-
-    private Funding 펀딩_생성(Member owner, LocalDate endDate) {
-        Funding funding = funding(
-                owner,
-                productRepository.save(product("", Price.from("1000000"))),
-                "10000",
-                endDate
-        );
-        fundingRepository.save(funding);
-        setField(funding, "status", EXPIRED);
-        fundingRepository.save(funding);
-        return funding;
     }
 }
