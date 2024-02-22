b86090b154e506e7d4c1d35d912e541279038b0c
Donghun Shin <huipulci1@naver.com>
2024-02-22 21:24:09 +0900

    [MOA-393] 펀딩 조회 시 내가 끝내기 한 비용이 반영되지 않는 부분 수정 (#62)
    * MOA-393: 펀딩 조회 시 내가 끝내기 한 비용이 반영되지 않는 부분 수정
    
    * MOA-393: 펀딩 목록 조회 시 진행비율 추가
    
    * MOA-393: 내가 끝내기로 채운 금액 DB 컬럼명 변경

--- api/src/main/java/moa/funding/query/response/FundingDetailResponse.java
+++ api/src/main/java/moa/funding/query/response/FundingDetailResponse.java
@@ -48,7 +48,7 @@ public record FundingDetailResponse(
         @Schema(description = "남은 금액", example = "140000")
         Long remainAmount,
 
-        @Schema(example = "56")
+        @Schema(description = "펀딩 달성 퍼센트", example = "56")
         int fundingRate,
 
         @Schema(description = "펀딩 상태", example = "PROCESSING")
