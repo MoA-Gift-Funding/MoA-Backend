232bc1941b79c73a1dc0a7724c72fb0cf4f72d3e
shin-mallang <huipulci1@naver.com>
2024-02-22 21:54:08 +0900

    MOA-397: 공지사항 API 문서 작성

--- api/src/main/java/moa/announcement/AnnouncementController.java
+++ api/src/main/java/moa/announcement/AnnouncementController.java
@@ -1,9 +1,12 @@
 package moa.announcement;
 
+import static moa.member.domain.MemberStatus.SIGNED_UP;
+
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import moa.announcement.query.AnnouncementQueryService;
 import moa.announcement.query.response.AnnouncementResponse;
+import moa.auth.Auth;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/announcements")
-public class AnnouncementController {
+public class AnnouncementController implements AnnouncementApi {
 
     private final AnnouncementQueryService announcementQueryService;
 
     @GetMapping
-    public ResponseEntity<List<AnnouncementResponse>> findAll() {
+    public ResponseEntity<List<AnnouncementResponse>> findAll(
+            @Auth(permit = SIGNED_UP) Long memberId
+    ) {
         List<AnnouncementResponse> result = announcementQueryService.findAll();
         return ResponseEntity.ok(result);
     }
