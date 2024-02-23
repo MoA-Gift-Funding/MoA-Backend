add8c8f91e809a97a5328241bfe737074011340b
shin-mallang <huipulci1@naver.com>
2024-02-23 20:19:11 +0900

    MOA-410: 펀딩 완료 시 주문 바로 생성되도록 변경

--- core/src/main/java/moa/order/domain/OrderStatus.java
+++ core/src/main/java/moa/order/domain/OrderStatus.java
@@ -2,5 +2,7 @@ package moa.order.domain;
 
 public enum OrderStatus {
 
-    RECEIVED
+    WAITING_RECEIVE,  // 수령대기 (기본적으로 수령완료 상태이나, 쿠폰 발급시 문제가 생긴 경우 해당 상태)
+    COMPLETE_RECEIVE,  // 수령완료
+    REFUND,  // 환불
 }
