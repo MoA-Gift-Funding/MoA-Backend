05da364ef7f3824a1f1fec16fb743bc017c02552
shin-mallang <huipulci1@naver.com>
2024-02-25 21:45:46 +0900

    MOA-399: 개발서버 실행 시 최초 상품 데이터 적재를 위한 임시 클래스 생성

--- batch/src/main/java/moa/MoaBatchApplication.java
+++ batch/src/main/java/moa/MoaBatchApplication.java
@@ -1,11 +1,19 @@
 package moa;
 
+import java.time.LocalDateTime;
 import java.util.TimeZone;
+import lombok.RequiredArgsConstructor;
 import moa.global.config.YamlPropertySourceFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 @PropertySource(
         value = {"classpath:application-core-${spring.profiles.active}.yml"},
@@ -18,5 +26,22 @@ public class MoaBatchApplication {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(MoaBatchApplication.class, args);
+    }
+
+    @Component
+    @RequiredArgsConstructor
+    public static class ProductInitializer implements CommandLineRunner {
+
+        private final JobLauncher jobLauncher;
+
+        private final Job wincubeProductUpdateJob;
+
+        @Override
+        public void run(String... args) throws Exception {
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addLocalDateTime("now", LocalDateTime.now())
+                    .toJobParameters();
+            jobLauncher.run(wincubeProductUpdateJob, jobParameters);
+        }
     }
 }
