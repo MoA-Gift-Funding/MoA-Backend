fd5b46e9486da575842f892285a9f3349b58d123
Donghun Shin <huipulci1@naver.com>
2024-02-28 19:07:38 +0900

    [MOA-467] 상용 서버에 상품 적재 코드 1회용 작성 (#96)

--- batch/src/main/java/moa/MoaBatchApplication.java
+++ batch/src/main/java/moa/MoaBatchApplication.java
@@ -1,11 +1,20 @@
 package moa;
 
+import java.time.LocalDateTime;
 import java.util.TimeZone;
+import lombok.RequiredArgsConstructor;
 import moa.global.config.YamlPropertySourceFactory;
+import moa.product.domain.ProductRepository;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 @PropertySource(
         value = {"classpath:application-core-${spring.profiles.active}.yml"},
@@ -18,5 +27,24 @@ public class MoaBatchApplication {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(MoaBatchApplication.class, args);
+    }
+
+    @Component
+    @RequiredArgsConstructor
+    public static class ProductInit implements CommandLineRunner {
+
+        private final JobLauncher jobLauncher;
+        private final Job wincubeProductUpdateJob;
+        private final ProductRepository productRepository;
+
+        @Override
+        public void run(String... args) throws Exception {
+            if (productRepository.findAll().isEmpty()) {
+                JobParameters jobParameters = new JobParametersBuilder()
+                        .addLocalDateTime("now", LocalDateTime.now())
+                        .toJobParameters();
+                jobLauncher.run(wincubeProductUpdateJob, jobParameters);
+            }
+        }
     }
 }
