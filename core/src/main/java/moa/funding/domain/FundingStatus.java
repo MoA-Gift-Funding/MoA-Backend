0d9c001018ec0580eb3e785b0b7146ee9c35cf69
Donghun Shin <huipulci1@naver.com>
2024-03-01 23:35:29 +0900

    [MOA-496] 펀딩에 환불완료 상태 추가 (#109)

--- core/src/main/java/moa/funding/domain/FundingStatus.java
+++ core/src/main/java/moa/funding/domain/FundingStatus.java
@@ -9,8 +9,7 @@ public enum FundingStatus {
     CANCELLED,  // 사용자가 취소함
     STOPPED,  // 상품 공급 중단으로 중단됨
     COMPLETE,
-
-    // 펀딩 기간 이후 7일동안 만료 상태 -> 금액 채우면 `완료`, 안채우고 7일이 지나거나, 취소하면 `취소`
     EXPIRED,
+    REFUND_COMPLETE,
     ;
 }
