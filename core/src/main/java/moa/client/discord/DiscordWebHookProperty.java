f38913c489584c44175af0953498cd51a1600f73
Donghun Shin <huipulci1@naver.com>
2024-03-02 18:00:11 +0900

    [MOA-500] 신고 접수시 디스코드로 알림 (#115)
    * MOA-500: 신고 접수 시 디스코드로 알림
    
    * MOA-500: 서브모듈 업데이트

--- core/src/main/java/moa/client/discord/DiscordWebHookProperty.java
+++ core/src/main/java/moa/client/discord/DiscordWebHookProperty.java
@@ -5,7 +5,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "discord")
 public record DiscordWebHookProperty(
         WebhookProperty inquiryChannel,
-        WebhookProperty errorChannel
+        WebhookProperty errorChannel,
+        WebhookProperty reportChannel
 ) {
     public record WebhookProperty(
             String webhookId,
